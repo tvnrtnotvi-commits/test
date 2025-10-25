04b0de064fda16dec334f42b7f4348845e9a387d
tvnrtnotvi-commits <satwiknath9@gmail.com>
2025-10-25 19:50:34 +0530

    Update plantapp.py

--- plantapp.py
+++ plantapp.py
@@ -10,6 +10,9 @@ w = st.number_input("Enter other side length (m):", min_value=0.0, value=5.0, st
 d = st.number_input("Enter spacing between plants (m):", min_value=0.1, value=1.0, step=0.1)
 
 st.markdown("##(not about trees)")
+from PIL import Image
+img=Image.open('plantapp.PNG')
+st.image(img)
 
 if st.button("Calculate"):
     if l <= 0 or w <= 0 or d <= 0:
@@ -57,4 +60,5 @@ if st.button("Calculate"):
     
 
        
+
 
