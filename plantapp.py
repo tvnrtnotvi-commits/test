fd19249103de78035f5c65e0e4bad7e90ae39591
tvnrtnotvi-commits <satwiknath9@gmail.com>
2025-10-26 10:47:24 +0530

    Update plantapp.py

--- plantapp.py
+++ plantapp.py
@@ -6,24 +6,15 @@ st.title("Plant Spacing")
 st.markdown("### Enter your land and spacing details:")
 st.markdown("###(not about plants)")
 
-l = st.number_input("Enter side length (m):", min_value=0.0, value=3.0, step=0.1)
-w = st.number_input("Enter other side length (m):", min_value=0.0, value=5.0, step=0.1)
+a = st.number_input("Enter side length (m):", min_value=0.0, value=3.0, step=0.1)
+b = st.number_input("Enter other side length (m):", min_value=0.0, value=5.0, step=0.1)
 d = st.number_input("Enter spacing between plants (m):", min_value=0.1, value=1.0, step=0.1)
 
 
 
 if st.button("Calculate"):
-    if l <= 0 or w <= 0 or d <= 0:
-        st.error("All values must be greater than 0.")
-    else:
-        if l > w:
-            a = l
-            b = w
-        else:
-            a = w
-            b = l
 
-        min_plants = (((math.floor(a/d)+1)*((math.floor((2*b)/(math.sqrt(3)*d)))+1))
+        min = (((math.floor(a/d)+1)*((math.floor((2*b)/(math.sqrt(3)*d)))+1))
                       - (math.floor(((math.floor((2*b)/(math.sqrt(3)*d)))+1)/2))
                       * (1-math.floor((a/d)-(math.floor(a/d)))))
 
@@ -31,32 +22,30 @@ if st.button("Calculate"):
                 - ((math.floor((math.floor((2*a)/(math.sqrt(3)*d)))+1)/2))
                 * (1-math.floor((a/d)-(math.floor(a/d)))))
 
-        if ((a/d)-math.floor(a/d) >= d/2):
-            result = min_plants
-            st.warning("Origin row along {b} m side will lead to {min_plants} plants only!")
-        else:
-            result = rev
-
-        if rev > min_plants:
+      
+        if rev > min:
             maxvalue = rev
         else:
-            maxvalue = min_plants
+            maxvalue = min
 
             
-        if rev < min_plants:
+        if rev < min:
             minvalue = rev
         else:
-            minvalue = min_plants
+            minvalue = min
 
 
         st.success(f"**{maxvalue:.0f}** is the maximum number of plants your land can accommodate")
 
-    if min_plants != rev:
-        st.success(f"For best use of area origin row should be along {a} m side.")
-        st.warning(f"Origin row along {b} m side will lead to {minvalue:.0f} plants only 🥀🥀.")
-       
-
-   
+     if rev>min :
+         st.success ("Your origin row should be along" ,b, "m side for best use of area.")
+         st.warning ("Origin row along" ,a, "m side will lead to",minvalue, "plants.")
+     
+    else:
+         st.success ("Your origin row should be along" ,a, "m side for best use of area.")
+     st.warning ("Origin row along" ,b, "m side will lead to ",minvalue, "plants.")
+     
+    
 
 
 from PIL import Image 
@@ -65,6 +54,7 @@ st.image(img)
 
 
        
+
 
 
 
